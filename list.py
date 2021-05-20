429dc7fd60f6b9142cec53b3db93e16b399787c8
Anugya <anugyapokharel2@gmail.com>
2021-05-20 18:17:39 +0545

    [Initial Commit]

---
+++ list.py
@@ -0,0 +1,14 @@
+set1={1,2,3,4,5,6,6,5}
+set2={1,3,4,5,6,6,7,7}
+print(set1)
+print(set2)
+set1=set1.union(set2)
+print(set1)
+
+dict={"Nepal":"Kathmandu","Japan":"Tokyo"}
+print(dict.keys())
+print(dict.values())
+
+for i in range(0,10):
+    print(i, end='s ')
+
